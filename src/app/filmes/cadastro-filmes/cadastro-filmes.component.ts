0f2cf63e056983ff7e76b2d88d51e99052ebd6ae
Pablo <bruli_pablo@hotmail.com>
2021-12-21 20:13:57 -0300

    [Feature Pablo - Task:Criando os componentes]

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ValidarCamposService } from "src/app/shared/components/campos/validar-campos.service";
 
 @Component({
-  selector: 'dio-cadastro-filmes',
-  templateUrl: './cadastro-filmes.component.html',
-  styleUrls: ['./cadastro-filmes.component.scss']
+  selector: "dio-cadastro-filmes",
+  templateUrl: "./cadastro-filmes.component.html",
+  styleUrls: ["./cadastro-filmes.component.scss"],
 })
 export class CadastroFilmesComponent implements OnInit {
+  cadastro: FormGroup;
 
-  options: FormGroup;
+  constructor( public validacao: ValidarCamposService,
+     private fb: FormBuilder) {}
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
-
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
-    });
-
+  get f(){
+    return this.cadastro.controls;
   }
 
+  ngOnInit(): void {
+    this.cadastro = this.fb.group({
+      titulo: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ["", [Validators.minLength(10)]],
+      dtLancamento: ["", [Validators.required]],
+      descricao: [""],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDB: ["", Validators.minLength(10)],
+      genero: ["", [Validators.required]],
+    });
+  }
+
+  salvar(): void{
+this.cadastro.markAllAsTouched();
+    if(this.cadastro.invalid){
+      return;
+    }
+
+    alert("SUCESSO!!\n\n" + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void{
+    this.cadastro.reset();
+  }
+  
 }
